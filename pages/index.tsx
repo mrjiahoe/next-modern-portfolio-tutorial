1f5f88eee00ac9eaee599e6a79f7dcb25321194a
jiahoe <mrjiahoe@hotmail.com>
2022-10-18 09:57:44 +0800

    build skills section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
@@ -30,6 +31,9 @@ const Home: NextPage = () => {
 			</section>
 
 			{/* Skills */}
+			<section id="skills" className="snap-start">
+				<Skills />
+			</section>
 
 			{/* Projects */}
 
