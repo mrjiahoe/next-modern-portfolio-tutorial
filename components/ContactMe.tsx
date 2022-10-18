409b3532c33dcb14bd44a599891583fee2cfe33c
jiahoe <mrjiahoe@hotmail.com>
2022-10-18 14:15:30 +0800

    add contact section

---
+++ components/ContactMe.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+
+type Props = {};
+
+function ContactMe({}: Props) {
+	return (
+		<div className="h-screen">
+			<div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+				<h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+					Contact
+				</h3>
+
+				<div className="flex flex-col space-y-10">
+					<h4 className="text-4xl font-semibold text-center">
+						I have got just what you need.{" "}
+						<span className="decoration-[#F7AB0A]/50 underline">
+							Lets Talk.
+						</span>
+					</h4>
+
+					<div className="space-y-10">
+						<div className="flex items-center space-x-5 justify-center">
+							<PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+							<p className="text-2xl">+1234567890</p>
+						</div>
+
+						<div className="flex items-center space-x-5 justify-center">
+							<EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+							<p className="text-2xl">user@email.com</p>
+						</div>
+
+						<div className="flex items-center space-x-5 justify-center">
+							<MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+							<p className="text-2xl">123 This Road</p>
+						</div>
+					</div>
+
+					<form className="flex flex-col space-y-2 w-fit mx-auto ">
+						<div className="flex space-x-2">
+							<input placeholder="Name" className="contactInput" type="text" />
+							<input
+								placeholder="Email"
+								className="contactInput"
+								type="email"
+							/>
+						</div>
+						<input placeholder="Subject" className="contactInput" type="text" />
+
+						<textarea placeholder="Message" className="contactInput" />
+						<button
+							type="submit"
+							className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+						>
+							Submit
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ContactMe;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import About from "../components/About";
+import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
@@ -42,6 +43,9 @@ const Home: NextPage = () => {
 			</section>
 
 			{/* Contact */}
+			<section id="contact" className="snap-start">
+				<ContactMe />
+			</section>
 		</div>
 	);
 };
