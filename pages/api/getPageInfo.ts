478b390413e48d2a96ca6c2a920d9920149d6437
jiahoe <mrjiahoe@hotmail.com>
2022-10-24 11:57:14 +0800

    creating type definitions

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -2,21 +2,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
-import { Social } from "../../typings";
+import { PageInfo } from "../../typings";
 
 const query = groq`
-*[_type == "social"]
+*[_type == "project"][0]
 `;
 
 type Data = {
-	socials: Social[];
+	pageInfo: PageInfo;
 };
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	const socials: Social[] = await sanityClient.fetch(query);
+	const pageInfo: PageInfo = await sanityClient.fetch(query);
 
-	res.status(200).json({ socials });
+	res.status(200).json({ pageInfo });
 }
